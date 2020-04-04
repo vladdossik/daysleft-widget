f68649c8ff887fe4e3b6295d6172dfbe760f92db
Vlad <vladdosiik7540@gmail.com>
2020-04-04 11:39:05 +0300

    done

---
+++ app/src/main/java/com/example/lab4/CountDays.java
@@ -0,0 +1,132 @@
+package com.example.lab4;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.TypedValue;
+import android.widget.RemoteViews;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class CountDays extends AppWidgetProvider {
+    public static final String ACTION_SCHEDULED_ALARM ="com.sd.lab4.SCHEDULED_ALARM";
+    private static final String ACTION_SCHEDULED_UPDATE ="com.sd.lab4.SCHEDULED_UPDATE";
+    private static final int DAY_OF_MONTH = (24 * 60 * 60 * 1000);
+    public static ComponentName getComponentName(Context context) {
+        return new ComponentName(context, CountDays.class);
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction()!= null) {
+            switch (intent.getAction()) {
+                case Intent.ACTION_BOOT_COMPLETED: // We need to update, as all alarms are cancelled after phone reboot
+                    // fall through
+                case Intent.ACTION_TIMEZONE_CHANGED:  // Self Explanatory
+                case Intent.ACTION_TIME_CHANGED:
+
+                case ACTION_SCHEDULED_UPDATE:
+                    AppWidgetManager manager = AppWidgetManager.getInstance(context);
+                    int[] ids = manager.getAppWidgetIds(getComponentName(context));
+                    onUpdate(context, manager, ids);
+                    break;
+
+                case ACTION_SCHEDULED_ALARM:
+                    showNotification(context);
+                    break;
+            }
+        }
+        super.onReceive(context,intent);
+    }
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            MainActivity.deleteDatePref(context, appWidgetId);
+        }
+    }
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.days_left);
+        views.setOnClickPendingIntent(R.id.contentFrame,pendingIntent);
+            Calendar calendar = Calendar.getInstance();
+            String widgetDate = MainActivity.loadDatePref(context, appWidgetId);
+            long timeInMilliseconds = 0l;
+            if (!widgetDate.equals("")) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                try {
+                    Date mDate = sdf.parse(widgetDate);
+                    timeInMilliseconds = mDate.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+            }
+            // Calculate the difference in days between now and event date
+            double diffInDays = (double) (timeInMilliseconds - calendar.getTimeInMillis()) / DAY_OF_MONTH;
+            int daysLeftCeil = (int) Math.ceil(diffInDays);
+            views.setTextViewText(R.id.counterTv, String.valueOf(Math.max(0, daysLeftCeil)));
+            if (daysLeftCeil == 0) {
+                scheduleAlarm(context, appWidgetId);
+                views.setTextViewText(R.id.counterTv, "0");
+            }
+        }
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+    private static void scheduleNextUpdate(Context context, int appWidgetId) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, CountDays.class).setAction(ACTION_SCHEDULED_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        long midnightTime = getTimeTillHour(0) + DAY_OF_MONTH;
+        alarmManager.cancel(pendingIntent);
+        alarmManager.set(AlarmManager.RTC, midnightTime, pendingIntent);
+    }
+    private static void scheduleAlarm(Context context, int appWidgetId) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, CountDays.class).setAction(ACTION_SCHEDULED_ALARM);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        alarmManager.cancel(pendingIntent);
+        long alarmTime = getTimeTillHour(9);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTime, pendingIntent);
+    }
+    private static long getTimeTillHour(int hour) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTimeInMillis();
+    }
+    private static void showNotification(Context context) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        String CHANNEL_ID = "alarm_ch_1";
+        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(context,CHANNEL_ID)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("Событие сегодня")
+                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(), 0))
+                .setAutoCancel(true);
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+}
+
---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,152 @@
+package com.example.lab4;
+
+import android.app.DatePickerDialog;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+public class MainActivity extends AppCompatActivity {
+    private static final String PREFS_NAME = "com.sd.lab4.CountdownW";
+    private static final String PREF_PREFIX_KEY = "appwidget";
+    private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private final Calendar mCalendar = Calendar.getInstance();
+    private EditText dateTv;
+    private TextView errorTv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Configure Widget");
+
+        // извлекаем ID конфигурируемого виджета
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        // формируем intent ответа
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+        // отрицательный ответ
+        setResult(RESULT_CANCELED,resultValue);
+
+        //проверяем корректность ID
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID){
+            finish();
+            return;
+        }
+        //Setup UI
+        errorTv = findViewById(R.id.errorTv);
+        dateTv = findViewById(R.id.dateTv);
+        dateTv.setText(loadDatePref(MainActivity.this, mAppWidgetId));
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, monthOfYear);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog(date);
+            }
+        });
+
+        Button applyBtn = findViewById(R.id.applyBtn);
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCalValue();
+
+                Calendar today = Calendar.getInstance();
+                today.set(Calendar.HOUR_OF_DAY, 0);
+                today.set(Calendar.MINUTE, 0);
+                today.set(Calendar.SECOND, 0);
+                today.set(Calendar.MILLISECOND, 0);
+                today.add(Calendar.DAY_OF_MONTH, 1);
+                if (mCalendar.before(today)) {
+
+                    errorTv.setText("Событие должно быть в будущем!");
+                    errorTv.setVisibility(View.VISIBLE);
+                } else {
+                    final Context context = MainActivity.this;
+
+                    // When the button is clicked, store the string locally
+                    saveDatePref(context, mAppWidgetId, dateTv.getText().toString(), false, false);
+                    // It is the responsibility of the configuration activity to update the app widget
+                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                    CountDays.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+                    // Make sure we pass back the original appWidgetId
+                    Intent resultValue = new Intent();
+                    resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                    setResult(RESULT_OK, resultValue);
+                    finish();
+                }
+            }
+        });
+    }
+    private void showDateDialog(DatePickerDialog.OnDateSetListener date) {
+        new DatePickerDialog(MainActivity.this, date,
+                mCalendar.get(Calendar.YEAR),
+                mCalendar.get(Calendar.MONTH),
+                mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void updateLabel() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        dateTv.setText(sdf.format(mCalendar.getTime()));
+        errorTv.setVisibility(View.GONE);
+    }
+    private void setCalValue() {
+        if (!dateTv.getText().toString().equals("")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            try {
+                mCalendar.setTime(sdf.parse(dateTv.getText().toString()));
+            } catch (ParseException e) {
+                // Invalid date in text view
+                e.printStackTrace();
+            }
+        }
+    }
+    static void saveDatePref(Context context, int appWidgetId, String text, boolean done, boolean shown) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
+        prefs.putBoolean(PREF_PREFIX_KEY + appWidgetId + "b", done);
+        prefs.putBoolean(PREF_PREFIX_KEY + appWidgetId + "n", shown);
+        prefs.apply();
+    }
+    static void deleteDatePref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId);
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId + "b");
+        prefs.remove(PREF_PREFIX_KEY + appWidgetId + "n");
+        prefs.apply();
+    }
+    static String loadDatePref(Context context, int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        String titleValue = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null);
+        if (titleValue != null) {
+            return titleValue;
+        } else {
+            return "";
+        }
+    }
+
+}
